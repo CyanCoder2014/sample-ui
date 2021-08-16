ca955d2c5fd9a6be3fcd4eb3eaca05ac56178bd6
farid-cyan <info@cyancoder.co>
2021-08-16 18:28:12 +0430

    dev delete method fo agenda

--- src/views/Agenda/Table.vue
+++ src/views/Agenda/Table.vue
@@ -5,17 +5,17 @@
     <template v-slot:header>
       <b-row align-v="center">
         <b-col>
-          <h3 class="mb-0">{{makeTitle(objectName)}} List</h3>
+          <h3 class="mb-0">{{ makeTitle(objectName) }} List</h3>
         </b-col>
         <b-col class="text-right">
-          <router-link :to="'/'+objectName+'/add'" class="btn btn-sm btn-primary">Add {{objectName}}</router-link>
+          <router-link :to="'/'+objectName+'/add'" class="btn btn-sm btn-primary">Add {{ objectName }}</router-link>
         </b-col>
       </b-row>
     </template>
 
 
     <b-table
-        v-if="items && items.length > 0"
+        v-if="items && items.length > 0 && !isLoading"
         :key="items.id"
         responsive
         :fields="fields"
@@ -24,94 +24,161 @@
       <template
           v-slot:cell(tittle)="data"
       >
-       <b>{{ data.item.title}}</b>
+        <b>{{ data.item.title }}</b>
+      </template>
+      <template
+          v-slot:cell(note)="data"
+      >
+        <small>{{ data.item.note }}</small>
+      </template>
+      <template
+          v-slot:cell(state)="data"
+      >
+        <b-badge variant="dark" v-text="stateText(data.item.state)"></b-badge>
       </template>
       <template
           v-slot:cell(actions)="data"
       >
         <b-button
             v-b-tooltip.hover
-            title="See plan"
+            title="Edit"
             class="btn-plan p2"
-            variant="primary"
+            variant="warning"
             size="sm"
+            @click="$router.push({ path: objectName+'/edit/'+data.item.id })"
         >
          <span class="svg-container">
-            <i class="fas fa-t"></i>
+            <i class="fas fa-edit"></i>
+         </span>
+        </b-button>
+        <b-button
+            v-b-tooltip.hover
+            title="Delete"
+            class="btn-plan p2"
+            variant="danger"
+            size="sm"
+            @click="deleteItem(data.item.id)"
+        >
+         <span class="svg-container">
+            <i class="fas fa-trash"></i>
          </span>
         </b-button>
       </template>
     </b-table>
 
 
-
   </b-card>
 </template>
 <script>
-  import { Table, TableColumn, DropdownMenu, DropdownItem, Dropdown} from 'element-ui'
-  export default {
-    name: 'list-table',
-    components: {
-      [Table.name]: Table,
-      [TableColumn.name]: TableColumn,
-      [Dropdown.name]: Dropdown,
-      [DropdownItem.name]: DropdownItem,
-      [DropdownMenu.name]: DropdownMenu,
-    },
+import {Table, TableColumn, DropdownMenu, DropdownItem, Dropdown} from 'element-ui'
 
-    props:{
-      objectName: '',
-      fields:[],
-    },
+export default {
+  name: 'list-table',
+  components: {
+    [Table.name]: Table,
+    [TableColumn.name]: TableColumn,
+    [Dropdown.name]: Dropdown,
+    [DropdownItem.name]: DropdownItem,
+    [DropdownMenu.name]: DropdownMenu,
+  },
 
-    data() {
-      return {
+  props: {
+    objectName: '',
+    fields: [],
+  },
 
-        isLoading:false,
-        currentPage:1,
-        items:[],
+  data() {
+    return {
+
+      isLoading: false,
+      currentPage: 1,
+      items: [],
+
+    }
+  },
+
+  created() {
+
+    this.getData()
+
+  },
+
+  methods: {
+
+    async getData() {
+
+      try {
+        // let params = {
+        //   page: this.currentPage
+        // }
+        this.isLoading = true
+        const response = await axios.get(this.objectName)
+
+        let data = response.data
+        this.items = data.data
+        this.total = data.total
+        this.perPage = data.per_page
+
+      } catch (err) {
+
+        this.$snotify.error(err);
+
+      } finally {
+
+        this.isLoading = false
 
       }
     },
 
-    created() {
-      this.getData()
-    },
+    async deleteItem(itemId) {
 
-    methods:{
-      async getData() {
+      if (confirm("Are sure to delete item?")) {
 
         try {
-          // let params = {
-          //   page: this.currentPage
-          // }
-
           // this.isLoading = true
-          const response = await axios.get('agenda')
-          let data = response.data
-          this.items = data.data
-          this.total = data.total
-          this.perPage = data.per_page
+          const response = await axios.delete(this.objectName + '/' + itemId)
+
+          this.getData()
 
         } catch (err) {
-          // this.$snotify.error(err);
+
+          this.$snotify.error(err);
+
         } finally {
+
           // this.isLoading = false
+
         }
-      },
-
-      makeTitle(string){
-        return string.charAt(0).toUpperCase() + string.slice(1)
       }
+    },
 
+    makeTitle(string) {
+      return string.charAt(0).toUpperCase() + string.slice(1)
+    },
+
+
+    stateText(item) {
+      switch(item) {
+        case '1':
+          return 'To do'
+          break;
+        case '2':
+          return 'Doing'
+          break;
+        case '3':
+          return 'Done'
+          break;
+        default:
+          return 'Unknown'
+      }
     },
 
 
 
+  },
 
 
-
-  }
+}
 </script>
 <style>
 </style>
